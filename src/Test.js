6c1ae8373e41e8243f5290ce0a7c6e8709efbd1e
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-08-06 11:27:26 -0400

    first commit

---
+++ src/Test.js
@@ -0,0 +1,14 @@
+export default class Test {
+    static dispo() {
+        // <div class="body" id="app">
+        //     <fieldset class="champ disponibilites" disabled="disabled">
+        //         <label style="font-weight: bold;" for="disponibilites">Disponibilités</label>
+        //         <label>Matin <input type="checkbox" name="disponibilites" id="disponibilites_matin" value="matin"></label>
+        //         <label>Midi <input type="checkbox" name="disponibilites" id="disponibilites_midi" value="midi"></label>
+        //         <label>Soir <input type="checkbox" name="disponibilites" id="disponibilites_soir" value="soir"></label>
+        //     </fieldset>
+        // </div>
+        var app, fieldset, label, input;
+        
+    }
+}
