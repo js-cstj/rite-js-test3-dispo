523c71f7ecc2aa47e75debb81b21d07a277706c2
Martin Boudreau <32491185+bobanum@users.noreply.github.com>
2020-11-12 20:13:53 -0500

    Solution

--- src/Test.js
+++ src/Test.js
@@ -9,6 +9,47 @@ export default class Test {
         //     </fieldset>
         // </div>
         var app, fieldset, label, input;
-        
+        app = document.getElementById("app");
+        fieldset = document.createElement("fieldset");
+        app.appendChild(fieldset);
+        fieldset.classList.add("champ");
+        fieldset.classList.add("disponibilites");
+        fieldset.setAttribute("disabled", "disabled");
+
+        var label = document.createElement("label");
+        fieldset.appendChild(label)
+        label.style.fontWeight = "bold";
+        label.setAttribute("for", "disponibilites");
+        label.innerHTML = "Disponibilités";
+
+        label = document.createElement("label");
+        fieldset.appendChild(label);
+        label.innerHTML = "Matin ";
+        input = document.createElement("input");
+        label.appendChild(input);
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("name", "disponibilites");
+        input.setAttribute("id", "disponibilites_matin");
+        input.setAttribute("value", "matin");
+
+        label = document.createElement("label");
+        fieldset.appendChild(label);
+        label.innerHTML = "Midi ";
+        input = document.createElement("input");
+        label.appendChild(input);
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("name", "disponibilites");
+        input.setAttribute("id", "disponibilites_midi");
+        input.setAttribute("value", "midi");
+
+        label = document.createElement("label");
+        fieldset.appendChild(label);
+        label.innerHTML = "Soir ";
+        input = document.createElement("input");
+        label.appendChild(input);
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("name", "disponibilites");
+        input.setAttribute("id", "disponibilites_soir");
+        input.setAttribute("value", "soir");
     }
 }
